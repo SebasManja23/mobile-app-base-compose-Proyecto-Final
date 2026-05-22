6ec7ff0c40b949517cf53bcdd4d818a4519ad205
SebasManja23 <s.manjarrez23@info.uas.edu.mx>
2026-05-21 18:06:48 -0700

    Initial commit: Setup base del proyecto y configuración de MaizApp

--- app/src/main/java/com/fic/mobile_app_base_compose/MainActivity.kt
+++ app/src/main/java/com/fic/mobile_app_base_compose/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.fic.mobile_app_base_compose.ui.theme.MobileappbasecomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileappbasecomposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "MaizApp - Cargando estructura...")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileappbasecomposeTheme {
-        Greeting("Android")
     }
 }
